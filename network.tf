16edf8222f0989812374b22804e6c023e25351e2
entishman <sharp.pnw@gmail.com>
2025-06-24 12:38:49 -0700

    profile

--- network.tf
+++ network.tf
@@ -1,3 +1,9 @@
 # -----------------------------------------------------------------------
 # Create VPC, IGW, subnets (public and private),  route tables (public and private) and routes.
 # -----------------------------------------------------------------------
+
+resource "google_compute_network" "network1" {
+  name                    = "gcp_trs_network"
+  auto_create_subnetworks = false
+  description             = "Network for GCP project"
+}
