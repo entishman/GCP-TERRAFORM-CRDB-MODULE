62a4a312edf8ff03c0763ec27aef2364d241ba9e
entishman <sharp.pnw@gmail.com>
2025-06-25 15:08:26 -0700

    profile

--- network.tf
+++ network.tf
@@ -4,6 +4,7 @@ resource "google_compute_network" "trs-network" {
   routing_mode            = "REGIONAL"
   description             = "Network for GCP project with static internal IP address"
 }
+
 resource "google_compute_subnetwork" "trs-public-subnetwork" {
   count         = 3
   name          = "trs-public-subnetwork-${count.index}"
@@ -11,7 +12,6 @@ resource "google_compute_subnetwork" "trs-public-subnetwork" {
   network       = google_compute_network.trs-network.id
   ip_cidr_range = local.public_subnet_list[count.index]
 }
-
 resource "google_compute_subnetwork" "trs-private-subnetwork" {
   count         = 3
   name          = "trs-private-subnetwork-${count.index}"
@@ -19,15 +19,27 @@ resource "google_compute_subnetwork" "trs-private-subnetwork" {
   network       = google_compute_network.trs-network.id
   ip_cidr_range = local.private_subnet_list[count.index]
 }
+
 resource "google_compute_address" "trs-static_public_internal_ip" {
   count        = 3
-  name         = "my-compute-static-internal-ip"                                   # Name for the static internal IP address resource
+  name         = "my-compute-public-static-internal-ip"                            # Name for the static internal IP address resource
   subnetwork   = google_compute_subnetwork.trs-public-subnetwork[count.index].name # Associate with the subnetwork
   address_type = "INTERNAL"                                                        # Specify that this is an internal IP
-  address      = "10.0.0.1"                                                        # The specific internal IP address to reserve
+  address      = private_subnet_list[count.index]                                  # The specific internal IP address to reserve
+  region       = "us-west2"                                                        # Must be in the same region as the subnetwork
+  description  = "Static internal IP for my public compute instances"
+}
+
+resource "google_compute_address" "trs-static_private_internal_ip" {
+  count        = 3
+  name         = "my-compute-prinate-static-internal-ip"                           # Name for the static internal IP address resource
+  subnetwork   = google_compute_subnetwork.trs-public-subnetwork[count.index].name # Associate with the subnetwork
+  address_type = "INTERNAL"                                                        # Specify that this is an internal IP
+  address      = public_subnet_list[count.index]                                   # The specific internal IP address to reserve
   region       = "us-west2"                                                        # Must be in the same region as the subnetwork
   description  = "Static internal IP for my compute instance"
 }
+
 resource "google_compute_route" "default_internet_route" {
   name             = "blah-my-default-internet-route"        # Name of the route
   dest_range       = "0.0.0.0/0"                             # Destination CIDR range (0.0.0.0/0 means all traffic)
@@ -37,13 +49,3 @@ resource "google_compute_route" "default_internet_route" {
   description      = "Route to the internet via default gateway"
 }
 
-
-/*
-resource "google_compute_address" "trs-address" {
-  name         = "trs-address"
-  region       = "us-west2"
-  address_type = "INTERNAL"
-  #network      = google_compute_network.trs-network.id
-  subnetwork = google_compute_subnetwork.trs-private-subnetwork.id
-}
-*/
