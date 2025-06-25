3af6edeae31ff5b4f9ef8785ee30c8bd109aa25b
Tim Sharp <78510844+entishman@users.noreply.github.com>
2025-06-24 18:33:42 -0700

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,9 +1,30 @@
-# -----------------------------------------------------------------------
-# Create VPC, IGW, subnets (public and private),  route tables (public and private) and routes.
-# -----------------------------------------------------------------------
-
-resource "google_compute_network" "network1" {
-  name                    = "gcp_trs_network"
+resource "google_compute_network" "trs-network" {
+  name                    = "trs-network"
   auto_create_subnetworks = false
   description             = "Network for GCP project"
 }
+
+resource "google_compute_subnetwork" "trs-public-subnetwork" {
+  count         = 3
+  name          = "trs-public-subnetwork-${count.index}"
+  region        = "us-west2"
+  network       = google_compute_network.trs-network.id
+  ip_cidr_range = local.public_subnet_list[count.index]
+}
+
+resource "google_compute_subnetwork" "trs-private-subnetwork" {
+  count         = 3
+  name          = "trs-private-subnetwork-${count.index}"
+  region        = "us-west2"
+  network       = google_compute_network.trs-network.id
+  ip_cidr_range = local.private_subnet_list[count.index]
+}
+/*
+resource "google_compute_address" "trs-address" {
+  name         = "trs-address"
+  region       = "us-west2"
+  address_type = "INTERNAL"
+  #network      = google_compute_network.trs-network.id
+  subnetwork = google_compute_subnetwork.trs-private-subnetwork.id
+}
+*/
