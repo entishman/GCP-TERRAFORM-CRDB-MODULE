b3d9a6e88bd9646122fc6ea14fafaefab4f1eadd
entishman <sharp.pnw@gmail.com>
2025-06-25 10:45:28 -0700

    profile

--- network.tf
+++ network.tf
@@ -19,12 +19,13 @@ resource "google_compute_subnetwork" "trs-private-subnetwork" {
   network       = google_compute_network.trs-network.id
   ip_cidr_range = local.private_subnet_list[count.index]
 }
-resource "google_compute_address" "trs-static_internal_ip" {
-  name         = "my-compute-static-internal-ip"                           # Name for the static internal IP address resource
-  subnetwork   = google_compute_subnetwork.trs-public-subnetwork.self_link # Associate with the subnetwork
-  address_type = "INTERNAL"                                                # Specify that this is an internal IP
-  address      = "10.0.0.1"                                                # The specific internal IP address to reserve
-  region       = "us-west2"                                                # Must be in the same region as the subnetwork
+resource "google_compute_address" "trs-static_public_internal_ip" {
+  count        = 3
+  name         = "my-compute-static-internal-ip"                                   # Name for the static internal IP address resource
+  subnetwork   = google_compute_subnetwork.trs-public-subnetwork.name[count.index] # Associate with the subnetwork
+  address_type = "INTERNAL"                                                        # Specify that this is an internal IP
+  address      = "10.0.0.1"                                                        # The specific internal IP address to reserve
+  region       = "us-west2"                                                        # Must be in the same region as the subnetwork
   description  = "Static internal IP for my compute instance"
 }
 resource "google_compute_route" "default_internet_route" {
