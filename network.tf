6946b40b01833ae6e7a40463b4f30e95fd7b7079
entishman <sharp.pnw@gmail.com>
2025-06-25 17:31:41 -0700

    profile

--- network.tf
+++ network.tf
@@ -20,7 +20,7 @@ resource "google_compute_subnetwork" "trs-private-subnetwork" {
   ip_cidr_range = local.private_subnet_list[count.index]
 }
 
-resource "google_compute_address" "trs-static_public_internal_ip" {
+resource "google_compute_address" "trs-static-public-internal-ip" {
   count        = 3
   name         = "my-compute-public-static-internal-ip-${count.index + 1}"         # Name for the static internal IP address resource
   subnetwork   = google_compute_subnetwork.trs-public-subnetwork[count.index].name # Associate with the subnetwork
@@ -30,7 +30,7 @@ resource "google_compute_address" "trs-static_public_internal_ip" {
   description = "Static internal IP for my public compute instances"
 }
 
-resource "google_compute_address" "trs-static_private_internal_ip" {
+resource "google_compute_address" "trs-static-private-internal-ip" {
   count        = 3
   name         = "my-compute-private-static-internal-ip-${count.index + 1}"         # Name for the static internal IP address resource
   subnetwork   = google_compute_subnetwork.trs-private-subnetwork[count.index].name # Associate with the subnetwork
@@ -40,11 +40,11 @@ resource "google_compute_address" "trs-static_private_internal_ip" {
   description = "Static internal IP for my compute instance"
 }
 
-resource "google_compute_route" "default_internet_route" {
+resource "google_compute_route" "default-internet-route" {
   name             = "blah-my-default-internet-route"        # Name of the route
   dest_range       = "0.0.0.0/0"                             # Destination CIDR range (0.0.0.0/0 means all traffic)
   network          = google_compute_network.trs-network.name # Name of the VPC network
-  next_hop_gateway = "projects/${google_compute_network.trs-network.project}/global/gateways/default-internet-gateway"
+  next_hop_gateway = "projects/${google-compute-network.trs-network.project}/global/gateways/default-internet-gateway"
   priority         = 1000 # Priority of the route (lower is higher priority)
   description      = "Route to the internet via default gateway"
 }
