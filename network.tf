b083ddbbe8d4d1af1d00281b75524ca6e04fc0bd
entishman <sharp.pnw@gmail.com>
2025-06-25 17:34:20 -0700

    profile

--- network.tf
+++ network.tf
@@ -35,7 +35,7 @@ resource "google_compute_address" "trs-static-private-internal-ip" {
   name         = "my-compute-private-static-internal-ip-${count.index + 1}"         # Name for the static internal IP address resource
   subnetwork   = google_compute_subnetwork.trs-private-subnetwork[count.index].name # Associate with the subnetwork
   address_type = "INTERNAL"                                                         # Specify that this is an internal IP
-  #address      = public_subnet_list[count.index]                                                # The specific internal IP address to reserve
+  #address      = private_subnet_list[count.index]                                                # The specific internal IP address to reserve
   region      = "us-west2" # Must be in the same region as the subnetwork
   description = "Static internal IP for my compute instance"
 }
