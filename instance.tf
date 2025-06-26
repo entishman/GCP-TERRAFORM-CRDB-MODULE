f075812c4f55fca21fcf7f20ab02c50968765efd
entishman <sharp.pnw@gmail.com>
2025-06-26 13:49:50 -0700

    profile

--- instance.tf
+++ instance.tf
@@ -10,3 +10,26 @@ resource "google_compute_firewall" "allow_ssh" {
 
   source_ranges = ["0.0.0.0/0"] # Allow from any IP address
 }
+
+resource "google_compute_instance" "trs-vm-instance" {
+  count        = 3
+  name         = "instance-name-${count.index}"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.trs-network.name
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "your-ssh-public-key"
+  }
+}
--- network.tf
+++ network.tf
@@ -4,41 +4,39 @@ resource "google_compute_network" "trs-network" {
   routing_mode            = "REGIONAL"
   description             = "Network for GCP project with static internal IP address"
 }
-
+#-------------------------------------------------------------------------------------
 resource "google_compute_subnetwork" "trs-public-subnetwork" {
   count         = 3
-  name          = "trs-public-subnetwork-${count.index}"
+  name          = "trs-public-subnetwork-${count.index + 1}"
   region        = "us-west2"
   network       = google_compute_network.trs-network.id
   ip_cidr_range = local.public_subnet_list[count.index]
 }
 resource "google_compute_subnetwork" "trs-private-subnetwork" {
   count         = 3
-  name          = "trs-private-subnetwork-${count.index}"
+  name          = "trs-private-subnetwork-${count.index + 1}"
   region        = "us-west2"
   network       = google_compute_network.trs-network.id
   ip_cidr_range = local.private_subnet_list[count.index]
 }
-
+#-------------------------------------------------------------------------------------
 resource "google_compute_address" "trs-static-public-internal-ip" {
   count        = 3
   name         = "my-compute-public-static-internal-ip-${count.index + 1}"         # Name for the static internal IP address resource
   subnetwork   = google_compute_subnetwork.trs-public-subnetwork[count.index].name # Associate with the subnetwork
   address_type = "INTERNAL"                                                        # Specify that this is an internal IP
-  #address      = private_subnet_list[count.index]                                 # The specific internal IP address to reserve
-  region      = "us-west2" # Must be in the same region as the subnetwork
-  description = "Static internal IP for my public compute instances"
+  region       = "us-west2"                                                        # Must be in the same region as the subnetwork
+  description  = "Static internal IP for my public compute instances"
 }
-
 resource "google_compute_address" "trs-static-private-internal-ip" {
   count        = 3
   name         = "my-compute-private-static-internal-ip-${count.index + 1}"         # Name for the static internal IP address resource
   subnetwork   = google_compute_subnetwork.trs-private-subnetwork[count.index].name # Associate with the subnetwork
   address_type = "INTERNAL"                                                         # Specify that this is an internal IP
-  #address      = private_subnet_list[count.index]                                                # The specific internal IP address to reserve
-  region      = "us-west2" # Must be in the same region as the subnetwork
-  description = "Static internal IP for my compute instance"
+  region       = "us-west2"                                                         # Must be in the same region as the subnetwork
+  description  = "Static internal IP for my compute instance"
 }
+#-------------------------------------------------------------------------------------
 /*
 resource "google_compute_route" "default-internet-route" {
   name             = "blah-my-default-internet-route"        # Name of the route
